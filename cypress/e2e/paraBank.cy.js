752fbebb295b3b368a7d1375b0d61b30120eae96
olaide.olatunji <olaide.olatunji@advancly.com>
2025-05-30 18:29:26 +0100

    updated code

--- cypress/e2e/paraBank.cy.js
+++ cypress/e2e/paraBank.cy.js
@@ -1,7 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://parabank.parasoft.com/parabank/index.htm')
-    cy.wait(10000);
+    cy.wait(2000);
     cy.contains('Register').click();
+    cy.get('#customer\\.firstName').type('Ajanlekoko')
+    cy.get('#customer\\.lastName').type('Olunlade')
   })
 })
